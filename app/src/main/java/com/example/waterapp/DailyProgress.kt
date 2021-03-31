7058bd8bd1ac8582290c41a076aaba118e1d3b50
Anthony R <70669368+aorom1997@users.noreply.github.com>
2021-03-30 22:01:15 -0400

    User Input

--- app/src/main/java/com/example/waterapp/DailyProgress.kt
+++ app/src/main/java/com/example/waterapp/DailyProgress.kt
@@ -1,11 +1,28 @@
 package com.example.waterapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_daily_progress.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class DailyProgress : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily_progress)
+
+
+        val Name = NameView;
+        val Age = AgeView;
+        val Weight = WeightView;
+
+        val name: String = intent.getStringExtra("name");
+        val age: String = intent.getStringExtra("age");
+        val weight: String = intent.getStringExtra("weight");
+
+        Name.text = "Hello $name!";
+        Age.text = age;
+        Weight.text = weight;
     }
 }
--- app/src/main/java/com/example/waterapp/MainActivity.kt
+++ app/src/main/java/com/example/waterapp/MainActivity.kt
@@ -4,17 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val nameInput = findViewById<EditText>(R.id.nameInput)
+        val ageInput = findViewById<EditText>(R.id.ageInput)
+        val weightInput = findViewById<EditText>(R.id.weightInput)
+
+
+
         val buttonDailyProgress= findViewById<ImageButton>(R.id.StartButton)
         buttonDailyProgress.setOnClickListener {
+            val name = nameInput.text.toString();
+            val age = ageInput.text.toString();
+            val weight = weightInput.text.toString();
+
+
             val intent = Intent( this , DailyProgress::class.java)
+            intent.putExtra("name", name);
+            intent.putExtra("age", age);
+            intent.putExtra("weight", weight);
             startActivity(intent);
+            finish();
         }
     }
 }
